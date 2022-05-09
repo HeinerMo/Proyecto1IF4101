5c6c941d4f1edc79dce0f2c5998dd1044928a131
Heiner <heinermongelopez@gmail.com>
2022-05-09 12:58:35 -0600

    Las imágenes abren una nueva ventana para mostrarse en mayor detalle

---
+++ js/openImage.js
@@ -0,0 +1,3 @@
+function openImage(img) {
+    window.open(img);
+}
--- js/updateDetails.js
+++ js/updateDetails.js
@@ -1,48 +1,47 @@
 function updateDetails(action) {
     let content = document.getElementById("info");
-    console.log(action);
     switch (action) {
         case '1uf':
-            content.innerHTML = `<img class="productImg" src="img/products/capacitors/${action}.jpg"></img>`;
+            content.innerHTML = `<a href="javascript:openImage('img/products/capacitors/${action}.jpg')"><img class="productImg" src="img/products/capacitors/${action}.jpg"></img></a><p>Capacitor 1 micro faradio</p>`;
             break;
         case '10uf':
-            content.innerHTML = `<img class="productImg" src="img/products/capacitors/${action}.jpg"></img>`;
+            content.innerHTML = `<a href="javascript:openImage('img/products/capacitors/${action}.jpg')""><img class="productImg" src="img/products/capacitors/${action}.jpg"></img></a><p>Capacitor 10 micro faradios</p>`;
             break;
         case '100uf':
-            content.innerHTML = `<img class="productImg" src="img/products/capacitors/${action}.jpg"></img>`;
+            content.innerHTML = `<a href="javascript:openImage('img/products/capacitors/${action}.jpg')""><img class="productImg" src="img/products/capacitors/${action}.jpg"></img></a><p>Capacitor 100 micro faradios</p>`;
             break;
         case '16ksram':
-            content.innerHTML = `<img class="productImg" src="img/products/cmos/${action}.jpg"></img>`;
+            content.innerHTML = `<a href="javascript:openImage('img/products/cmos/${action}.jpg')""><img class="productImg" src="img/products/cmos/${action}.jpg"></img></a><p>RAM estática 16k CMOS</p>`;
             break;
         case '64ksram':
-            content.innerHTML = `<img class="productImg" src="img/products/cmos/${action}.jpg"></img>`;
+            content.innerHTML = `<a href="javascript:openImage('img/products/cmos/${action}.jpg')""><img class="productImg" src="img/products/cmos/${action}.jpg"></img></a><p>RAM estática 64k CMOS</p>`;
             break;
         case '5vdc':
-            content.innerHTML = `<img class="productImg" src="img/products/motors/${action}.jpg"></img>`;
+            content.innerHTML = `<a href="javascript:openImage('img/products/motors/${action}.jpg')""><img class="productImg" src="img/products/motors/${action}.jpg"></img></a><p>Motor DC 5v</p>`;
             break;
         case '12vdc':
-            content.innerHTML = `<img class="productImg" src="img/products/motors/${action}.jpg"></img>`;
+            content.innerHTML = `<a href="javascript:openImage('img/products/motors/${action}.jpg')""><img class="productImg" src="img/products/motors/${action}.jpg"></img></a><p>Motor DC 12v</p>`;
             break;
         case '24vdc':
-            content.innerHTML = `<img class="productImg" src="img/products/motors/${action}.jpg"></img>`;
+            content.innerHTML = `<a href="javascript:openImage('img/products/motors/${action}.jpg')""><img class="productImg" src="img/products/motors/${action}.jpg"></img></a><p>Motor DC 12v</p>`;
             break;
         case '1k':
-            content.innerHTML = `<img class="productImg" src="img/products/resistors/${action}.jpg"></img>`;
+            content.innerHTML = `<a href="javascript:openImage('img/products/resistors/${action}.jpg')""><img class="productImg" src="img/products/resistors/${action}.jpg"></img></a><p>Resistencia 1k Ohm</p>`;
             break;
         case '10k':
-            content.innerHTML = `<img class="productImg" src="img/products/resistors/${action}.jpg"></img>`;
+            content.innerHTML = `<a href="javascript:openImage('img/products/resistors/${action}.jpg')""><img class="productImg" src="img/products/resistors/${action}.jpg"></img></a><p>Resistencia 10k Ohm</p>`;
             break;
         case '100k':
-            content.innerHTML = `<img class="productImg" src="img/products/resistors/${action}.jpg"></img>`;
+            content.innerHTML = `<a href="javascript:openImage('img/products/resistors/${action}.jpg')""><img class="productImg" src="img/products/resistors/${action}.jpg"></img></a><p>Resistencia 100k Ohm</p>`;
             break;
         case 'and':
-            content.innerHTML = `<img class="productImg" src="img/products/ttl/${action}.jpg"></img>`;
+            content.innerHTML = `<a href="javascript:openImage('img/products/ttl/${action}.jpg')""><img class="productImg" src="img/products/ttl/${action}.jpg"></img></a><p>Compuerta AND 4x TTL</p>`;
             break;
         case 'nand':
-            content.innerHTML = `<img class="productImg" src="img/products/ttl/${action}.jpg"></img>`;
+            content.innerHTML = `<a href="javascript:openImage('img/products/ttl/${action}.jpg')""><img class="productImg" src="img/products/ttl/${action}.jpg"></img><p>Compuerta NAND 4x TTL</p>`;
             break;
         case 'not':
-            content.innerHTML = `<img class="productImg" src="img/products/ttl/${action}.jpg"></img>`;
+            content.innerHTML = `<a href="javascript:openImage('img/products/ttl/${action}.jpg')""><img class="productImg" src="img/products/ttl/${action}.jpg"></img><p>Compuerta NOT 8x TTL</p>`;
             break;
         default:
             console.log(action);
