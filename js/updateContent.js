1fe90e5f4936e1a9716e4144e9d6910bad2ec0b1
Heiner <heinermongelopez@gmail.com>
2022-05-09 22:34:12 -0600

    Refactor para utilizar switch en lugar de if-else

--- js/updateContent.js
+++ js/updateContent.js
@@ -1,62 +1,62 @@
 function update(action) {
     let section = document.getElementById("information");
 
-    if (action == "capacitors") {
 
-        let items = ["1uf"
-            , "10uf"
-            , "100uf"];
+    switch (action) {
+        case "capacitors":
+            items = ["1uf"
+                , "10uf"
+                , "100uf"];
 
-        result = "<h2>Capacitores</h2><div class='products'>"
-        for (i = 0; i < items.length; i++) {
-            result += `<a href=javascript:updateDetails("${items[i]}")>${items[i]}</a>`;
-        }
-        section.innerHTML = result += "</div>";
+            result = "<h2>Capacitores</h2><div class='products'>"
+            for (i = 0; i < items.length; i++) {
+                result += `<a href=javascript:updateDetails("${items[i]}")>${items[i]}</a>`;
+            }
+            section.innerHTML = result += "</div>";
+            break;
+        case "resistors":
+            items = ["1k"
+                , "10k"
+                , "100k"];
 
-    } else if (action == "resistors") {
+            result = "<h2>Resistencias</h2><div class='products'>"
+            for (i = 0; i < items.length; i++) {
+                result += `<a href=javascript:updateDetails("${items[i]}")>${items[i]}</a>`;
+            }
+            section.innerHTML = result += "</div>";
+            break;
+        case "ttl":
+            items = ["and"
+                , "nand"
+                , "not"];
 
-        let items = ["1k"
-            , "10k"
-            , "100k"];
+            result = "<h2>Lógica TTL</h2><div class='products'>"
+            for (i = 0; i < items.length; i++) {
+                result += `<a href=javascript:updateDetails("${items[i]}")>${items[i]}</a>`;
+            }
+            section.innerHTML = result += "</div>";
+            break;
+        case "cmos":
+            items = ["16ksram"
+                , "64ksram"];
 
-        result = "<h2>Resistencias</h2><div class='products'>"
-        for (i = 0; i < items.length; i++) {
-            result += `<a href=javascript:updateDetails("${items[i]}")>${items[i]}</a>`;
-        }
-        section.innerHTML = result += "</div>";
+            result = "<h2>Lógica CMOS</h2><div class='products'>"
+            for (i = 0; i < items.length; i++) {
+                result += `<a href=javascript:updateDetails("${items[i]}")>${items[i]}</a>`;
+            }
+            section.innerHTML = result += "</div>";
+            break;
+        case "motors":
+            items = ["5vdc"
+                , "12vdc"
+                , "24vdc"];
 
-    } else if (action == "ttl") {
-        let items = ["and"
-            , "nand"
-            , "not"];
-
-        result = "<h2>Lógica TTL</h2><div class='products'>"
-        for (i = 0; i < items.length; i++) {
-            result += `<a href=javascript:updateDetails("${items[i]}")>${items[i]}</a>`;
-        }
-        
-        section.innerHTML = result += "</div>";
-
-    } else if (action == "cmos") {
-        let items = ["16ksram"
-            , "64ksram"];
-
-        result = "<h2>Lógica CMOS</h2><div class='products'>"
-        for (i = 0; i < items.length; i++) {
-            result += `<a href=javascript:updateDetails("${items[i]}")>${items[i]}</a>`;
-        }
-        section.innerHTML = result += "</div>";
-
-    } else if (action == "motors") {
-        let items = ["5vdc"
-            , "12vdc"
-            , "24vdc"];
-
-        result = "<h2>Motores</h2><div class='products'>"
-        for (i = 0; i < items.length; i++) {
-            result += `<a href=javascript:updateDetails("${items[i]}")>${items[i]}</a>`;
-        }
-        section.innerHTML = result += "</div>";
-
+            result = "<h2>Motores</h2><div class='products'>"
+            for (i = 0; i < items.length; i++) {
+                result += `<a href=javascript:updateDetails("${items[i]}")>${items[i]}</a>`;
+            }
+            section.innerHTML = result += "</div>";
+            break;
     }
+
 }
